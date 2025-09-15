277be23875c24508c6d38642637cf32f3eedaa5b
Renata I. Tagirovna <renata.tag.isl@gmail.com>
2025-09-15 15:20:34 +0300

    Add subtract function

---
+++ HW2_Islamova/calculator.py
@@ -0,0 +1,24 @@
+# calculator.py
+
+def add(a, b):
+    # сложение
+    pass
+
+def subtract(a, b):
+    # вычитание
+    pass
+
+def multiply(a, b):
+    # умножение
+    pass
+
+def divide(a, b):
+    # деление
+    pass
+
+def main():
+    # ввод данных и вызов нужной функции
+    pass
+
+if __name__ == "__main__":
+    main()
