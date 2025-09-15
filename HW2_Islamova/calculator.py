79c25699a1ac488c3f3a0cf2afc5bc6e5db3a566
Renata I. Tagirovna <renata.tag.isl@gmail.com>
2025-09-16 02:18:39 +0300

    Add management functions (main)

--- HW2_Islamova/calculator.py
+++ HW2_Islamova/calculator.py
@@ -17,8 +17,30 @@ def divide(a, b):
     pass
 
 def main():
-    # ввод данных и вызов нужной функции
-    pass
+    # entering data and calling the required function
+     # The main function
+    expression = input("Enter an expression, for example 1+3 : ")  # Get string from user
+    parts = expression.split()  #  Break the string by space-> ['1', '+', '3']
+    a = float(parts[0])         # Convert first element to number
+    operator = parts[1]         # The second element is the operator
+    b = float(parts[2])         # Third element - second number
 
+    # Depending on the operator, we call the appropriate function
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = subtract(a, b)
+    elif operator == '*':
+        result = multiply(a, b)
+    elif operator == '/':
+        result = divide(a, b)
+    else:
+        print("Unknown operator!")
+        return
+
+    print(result) # Print result
+    pass
+# This block ensures that the main function is called only 
+# when the script is run directly.
 if __name__ == "__main__":
     main()
