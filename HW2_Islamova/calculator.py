a9edfd15fdd69e34bc8000c2a9d453a2394ddebc
Dora Phillips <dora.phillips67@gmail.com>
2025-09-17 19:48:09 +0700

    insert add func, refactor main logic

--- HW2_Islamova/calculator.py
+++ HW2_Islamova/calculator.py
@@ -1,8 +1,10 @@
 # calculator.py
 
+AVAILABLE_OPERATORS = ['+', '-', '*', '/']
+
 def add(a, b):
-    # сложение
-    pass
+    result = a+b
+    return result
 
 def subtract(a, b):
     # вычитание
@@ -18,12 +20,27 @@ def divide(a, b):
 
 def main():
     # entering data and calling the required function
-     # The main function
-    expression = input("Enter an expression, for example 1+3 : ")  # Get string from user
-    parts = expression.split()  #  Break the string by space-> ['1', '+', '3']
-    a = float(parts[0])         # Convert first element to number
-    operator = parts[1]         # The second element is the operator
-    b = float(parts[2])         # Third element - second number
+    # The main function
+    # expression = input("Enter an expression, for example 1+3 : ")  # Get string from user
+    # TODO: есть ошибки в цикле, нужно исправить
+    condition = True
+    while condition: 
+        a = input("Input first number: ")
+        operator = str(input("Enter math operation: "))
+        b = input("Input second operator: ")
+
+        # TODO: нужен более корректный метод проверки на число: целое, с плавающей точкой, и отрицательное
+        if a.isnumeric() and b.isnumeric() and operator in AVAILABLE_OPERATORS:
+            a, b = float(a), float(b)
+            condition = False
+        else:
+            print("Input natural number")
+   
+    # TODO: на удаление, если подать 1234+7484, то будет ошибка
+    #parts = expression.split()  #  Break the string by space-> ['1', '+', '3']
+    #a = float(parts[0])         # Convert first element to number
+    #operator = parts[1]         # The second element is the operator
+    #b = float(parts[2])         # Third element - second number
 
     # Depending on the operator, we call the appropriate function
     if operator == '+':
