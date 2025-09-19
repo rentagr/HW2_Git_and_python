95c302a173a062a6d057fdead6d89649074c4b34
Mikhail-Dobryakov <misha0403228@gmail.com>
2025-09-19 18:43:27 +0300

    fix devide output

--- HW2_Islamova/calculator.py
+++ HW2_Islamova/calculator.py
@@ -19,8 +19,8 @@ def divide(a, b):
     if b == 0:
         print("Error: Division by zero is not possible")
         return None
-    
-    return a / b
+    result = a / b
+    return result
 
 def main():
     # entering data and calling the required function
