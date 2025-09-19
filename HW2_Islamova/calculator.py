1b79203f162a3bd4a55683b36a50b68349dbfa3c
Dora Phillips <dora.phillips67@gmail.com>
2025-09-20 00:13:00 +0700

    remowed TODOs

--- HW2_Islamova/calculator.py
+++ HW2_Islamova/calculator.py
@@ -31,25 +31,18 @@ def main():
     # entering data and calling the required function
     # The main function
     # expression = input("Enter an expression, for example 1+3 : ")  # Get string from user
-    # TODO: есть ошибки в цикле, нужно исправить
     condition = True
     while condition: 
         a = input("Input first number: ")
         operator = input("Enter math operation: ")
         b = input("Input second number: ")
 
-        # TODO: нужен более корректный метод проверки на число: целое, с плавающей точкой, и отрицательное
         if is_number(a) and is_number(b) and operator in AVAILABLE_OPERATORS:
             a, b = float(a), float(b)
             condition = False
         else:
             print("Please, input valid numbers and operator (+,-,*,/)")
    
-    # TODO: на удаление, если подать 1234+7484, то будет ошибка
-    #parts = expression.split()  #  Break the string by space-> ['1', '+', '3']
-    #a = float(parts[0])         # Convert first element to number
-    #operator = parts[1]         # The second element is the operator
-    #b = float(parts[2])         # Third element - second number
 
     # Depending on the operator, we call the appropriate function
     if operator == '+':
@@ -70,4 +63,3 @@ def main():
 # when the script is run directly.
 if __name__ == "__main__":
     main()
-
